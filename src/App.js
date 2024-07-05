23fb48e39f69cf36c773677e712541f86a8a20f8
AlesiaCikalleshi <118731784+AlesiaCikalleshi@users.noreply.github.com>
2024-07-05 23:52:22 +0200

    ui styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { theme } from "styles/Theme";
+import { GlobalStyles } from "styles/Global";
+import { MainTitle, SubText, SubTitle, SectionTitle, Button, Text } from "components/ui/typography";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Heart,
+  Music,
+  Pause,
+  Play,
+  Search,
+  SkipLeft,
+  SkipRight,
+  Users,
+  Volume,
+} from "components/ui/Icons";
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <MainTitle>Hello</MainTitle>
+      <SectionTitle>Hello</SectionTitle>
+      <SubTitle>Hello</SubTitle>
+      <Text>Hello</Text>
+      <SubText>Hello</SubText>
+      <Button>Hello</Button>
+      <br />
+      <Play />
+      <SkipLeft />
+      <SkipRight />
+      <Pause />
+      <Volume />
+      <Music />
+      <ArrowLeft />
+      <ArrowRight />
+      <Heart />
+      <Users />
+      <Search />
+    </ThemeProvider>
+  );
 }
 
 export default App;
